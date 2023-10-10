370c4d7aad66456627c2dd6089dc5abdf1b97b91
Krzysztof Len <len.krzysztof@gmail.com>
2023-10-10 21:02:48 +0200

    feat(code): change the commit message code

--- git_hooks/commit_message.dart
+++ git_hooks/commit_message.dart
@@ -1,20 +1,45 @@
 // ignore_for_file: avoid_print
 import 'dart:io';
 
-dynamic main() {
-  final rootDir = Directory.current;
-  final commitFile = File("${rootDir.path}/.git/COMMIT_EDITMSG");
-  final commitMessage = commitFile.readAsStringSync();
+// dynamic main() {
+//   final rootDir = Directory.current;
+//   final commitFile = File("${rootDir.path}/.git/COMMIT_EDITMSG");
+//   final commitMessage = commitFile.readAsStringSync();
 
-  final regExp = RegExp(
-    r'(fix|feat|chore|docs)\:.+',
-  );
+//   final regExp = RegExp(
+//     r'(fix|feat|chore|docs)\:.+',
+//   );
 
-  final valid = regExp.hasMatch(commitMessage);
-  if (!valid) {
-    print('''👎 Bad commit message!''');
-    exitCode = 1;
-  } else {
-    print('''👍 Nice commit message dude!''');
+//   final valid = regExp.hasMatch(commitMessage);
+//   if (!valid) {
+//     print('''👎 Bad commit message!''');
+//     exitCode = 1;
+//   } else {
+//     print('''👍 Nice commit message dude!''');
+//   }
+// }
+
+void main() {
+  final commitMessageFile = File('.git/COMMIT_EDITMSG');
+
+  if (!commitMessageFile.existsSync()) {
+    print('Commit message file does not exist.');
+    exit(1);
   }
+
+  final commitMessage = commitMessageFile.readAsStringSync();
+
+  if (!isValidCommitMessage(commitMessage)) {
+    print('👎 Invalid commit message format.');
+    print('Commit message should follow the Conventional Commits format.');
+    exit(1);
+  }
+
+  print('''👍 Valid commit message!''');
+  exit(0);
+}
+
+bool isValidCommitMessage(String commitMessage) {
+  final RegExp pattern = RegExp(r'^(feat|fix|docs|chore)(\(.+\))?: .+');
+  return pattern.hasMatch(commitMessage);
 }
