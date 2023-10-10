22727403abf8229826f797c9a3902b62725e02ba
Krzysztof Len <len.krzysztof@gmail.com>
2023-10-10 21:03:06 +0200

    feat(code): change the commit message code

--- git_hooks/commit_message.dart
+++ git_hooks/commit_message.dart
@@ -1,24 +1,6 @@
 // ignore_for_file: avoid_print
 import 'dart:io';
 
-// dynamic main() {
-//   final rootDir = Directory.current;
-//   final commitFile = File("${rootDir.path}/.git/COMMIT_EDITMSG");
-//   final commitMessage = commitFile.readAsStringSync();
-
-//   final regExp = RegExp(
-//     r'(fix|feat|chore|docs)\:.+',
-//   );
-
-//   final valid = regExp.hasMatch(commitMessage);
-//   if (!valid) {
-//     print('''👎 Bad commit message!''');
-//     exitCode = 1;
-//   } else {
-//     print('''👍 Nice commit message dude!''');
-//   }
-// }
-
 void main() {
   final commitMessageFile = File('.git/COMMIT_EDITMSG');
 
